30025c43590590caea4dc010806254a0dbed564d
lgx520 <982392354@qq.com>
2018-12-22 10:35:08 +0800

    2018-12-22

---
+++ iTrip-utils/src/main/java/org/itrip/utils/ProductRandomUtil.java
@@ -0,0 +1,29 @@
+package org.itrip.utils;
+
+import java.util.Random;
+
+/**
+ * 生成订单号
+ * @author Administrator
+ *
+ */
+public class ProductRandomUtil {
+	/**
+	 * 生成随机订单号
+	 * @return
+	 */
+	public String productNo() {
+		StringBuffer sb = new StringBuffer();
+		char[] A_z = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        Random r = new Random(); 
+        for(int i=0;i<4;i++) {
+        	 int sub = r.nextInt(A_z.length);
+        	sb.append(A_z[sub]);	
+        }
+        for(int y=0;y<13;y++) {
+        	int num = (int)(Math.random()*10+1);
+        	sb.append(num);
+        }
+        return sb.toString();
+	}
+}
